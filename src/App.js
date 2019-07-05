938477b5aad27a03ed17562addddffb530a309f8
tryingtokeepup <kl.hawaii@gmail.com>
2019-07-05 15:00:05 -0700

    i got state to render what I needed it to! Now, to fix the input component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 import Authentication from './components/auth.js';
+import CoinForm from './components/form';
 import DEXAG from 'dexag-sdk';
 
 const sdk = DEXAG.fromProvider(window.ethereum);
@@ -28,19 +29,27 @@ const GlobalStyle = createGlobalStyle`
 const MainPageDiv = styled.div`
   border: 1px solid white;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   min-height: 100vh;
 `;
-const name = 'placeholder';
+const name = 'User';
 const orderModel = {
   metadata: {
     source: {}
   }
 };
 
-// console.log('trial promise = ', findTrade());
-
+const MainContainerDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 300px;
+  min-width: 300px;
+  border: 1px solid white;
+`;
 class App extends Component {
   constructor(props) {
     super(props);
@@ -57,15 +66,24 @@ class App extends Component {
   }
 
   findTrade = async () => {
+    let { tokenPair, type } = this.state;
     const trade = await sdk.getTrade({
-      to: 'cDAI',
-      from: 'ETH',
+      to: tokenPair.to,
+      from: tokenPair.from,
       toAmount: 1,
       dex: 'Best'
     });
     this.setState({ ...this.state, order: trade });
   };
-
+  changeToken = (newType, token) => {
+    let tokenPair = this.state.tokenPair;
+    this.setState({ order: orderModel });
+    // let the user change the token type
+    tokenPair[newType] = token;
+    this.setState({ tokenPair: tokenPair }, () => {
+      this.findTrade();
+    });
+  };
   componentDidMount() {
     sdk.registerStatusHandler((status, data) => {
       this.setState({ web3Status: { status, data } });
@@ -80,6 +98,7 @@ class App extends Component {
 
   render() {
     console.log('trial = ', this.state.order);
+    console.log(this.state.tokenPair);
     return (
       <MainPageDiv>
         <GlobalStyle />
@@ -87,6 +106,10 @@ class App extends Component {
           <h1>Hey {name}, thanks for signing up!</h1>
           <p>Thank you for logging in!</p>
         </header>
+        <MainContainerDiv>
+          <p>1 cDAI = {this.state.order.metadata.source.price} ETH</p>
+          <CoinForm tokenPair={this.state.tokenPair} />
+        </MainContainerDiv>
       </MainPageDiv>
     );
   }
--- src/components/form.js
+++ src/components/form.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 /********************************************** Styles ********************************************/
 const DivWrapper = styled.div``;
 
 /********************************************* Component ******************************************/
-const CoinForm = props => {
-  return (
-    <DivWrapper>
-      <h1>This is the CoinForm component</h1>
-    </DivWrapper>
-  );
-};
 
-export default CoinForm;
+export default class CoinForm extends Component {
+  //console.log("i'm here in form", props);
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <DivWrapper>
+        <select default={this.state.tokenPair}>
+          <option value="ticker1">{this.props.tokenPair.to}</option>
+          <option value="ticker2">{this.props.tokenPair.from}</option>
+          <option value="ticker3">something</option>
+        </select>
+      </DivWrapper>
+    );
+  }
+}
